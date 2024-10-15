279a84ae6944298780b33656f167c3b04b7b6354
sandip biradi <sanbiradi@gmail.com>
2024-10-16 01:22:42 +0530

    test passed - Handle two comma-seperated numbers in add()

--- src/calculate.test.ts
+++ src/calculate.test.ts
@@ -6,12 +6,17 @@ describe('String Calculator', () => {
     calculator = new Calculator();
   });
 
-  // test('should return 0 for an empty string', () => {
-  //   expect(calculator.add('')).toBe(0);
-  // });
+  test('should return 0 for an empty string', () => {
+    expect(calculator.add('')).toBe(0);
+  });
   
   test('should return the number when only one number is provided', () => {
     expect(calculator.add('1')).toBe(1);
   });
+
+  test('should return the sum of two comma-separated numbers', () => {
+    expect(calculator.add('1,2')).toBe(3);
+  });
+  
   
 });
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,7 @@
 export class Calculator {
   add(numbers: string): number {
     if (numbers === '') return 0;
-    return parseInt(numbers);
-  }
+    const nums = numbers.split(',');
+    return nums.map(Number).reduce((a, b) => a + b, 0);
+  }  
 }
