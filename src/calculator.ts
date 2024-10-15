3fa40f12f3238945bce28b8ccb206634c04083ca
sandip biradi <sanbiradi@gmail.com>
2024-10-16 01:28:22 +0530

    test passed - handle new lines as well as handle different delimeters in add()

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,16 @@
 export class Calculator {
   add(numbers: string): number {
     if (numbers === '') return 0;
-    const nums = numbers.split(',');
+  
+    let delimiter = /,|\n/;
+    if (numbers.startsWith('//')) {
+      const delimiterEnd = numbers.indexOf('\n');
+      delimiter = new RegExp(numbers[2]);
+      numbers = numbers.substring(delimiterEnd + 1);
+    }
+  
+    const nums = numbers.split(delimiter);
     return nums.map(Number).reduce((a, b) => a + b, 0);
-  }  
+  }
+   
 }
